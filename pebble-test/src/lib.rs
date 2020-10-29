cbbe895176ef610e08bffce7436567050deb9b3d
Tamme Schichler <tamme@schichler.dev>
2020-10-30 00:49:57 +0100

    Revert ""miles to see you"-sys"
    This reverts commit c55177b7ee8306cb833322d765e96a37d6bfc400.

--- pebble-test/src/lib.rs
+++ pebble-test/src/lib.rs
@@ -1,38 +1,29 @@
 #![no_std]
 #![warn(clippy::pedantic)]
 
-use pebble_sys::{
-	foundation::app::app_event_loop,
-	standard_c::memory::{c_str, void},
-	user_interface::{
-		window::number_window::{
-			number_window_create, number_window_get_window_mut, number_window_set_value,
-			NumberWindowCallbacks,
-		},
-		window_stack::window_stack_push,
-	},
+use debugless_unwrap::DebuglessUnwrap as _;
+use pebble::{
+	foundation::app,
+	standard_c::CStr,
+	user_interface::window::number_window::{NumberWindow, NumberWindowData},
 };
 
 #[no_mangle]
 pub extern "C" fn main() -> i32 {
-	static mut CONTEXT: () = ();
-
-	unsafe {
-		let label = &*("miles to see you\0" as *const _ as *const c_str);
-		let number_window = number_window_create(
-			label,
-			NumberWindowCallbacks {
-				incremented: None,
-				decremented: None,
-				selected: None,
-			},
-			&mut *(&mut CONTEXT as *mut _ as *mut void),
-		)
-		.unwrap();
-		number_window_set_value(number_window, 10_000);
-		let window = number_window_get_window_mut(number_window);
-		window_stack_push(window, true);
-		app_event_loop();
-		0
-	}
+	let label = CStr::try_from_static("miles to see you\0").unwrap();
+	let number_window = NumberWindow::new(
+		&label,
+		NumberWindowData {
+			incremented: |_, _| (),
+			decremented: |_, _| (),
+			selected: |_, _| (),
+			context: (),
+		},
+	)
+	.debugless_unwrap();
+	number_window.set_value(10_000);
+	let window = number_window.window();
+	window.show(true);
+	app::event_loop();
+	0
 }
