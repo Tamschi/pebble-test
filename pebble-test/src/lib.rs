bf2bea1e7ff1b3df9d311e1fad127e8f959703a5
Tamme Schichler <tamme@schichler.dev>
2020-10-30 22:00:22 +0100

    Added a small image of unsafe!Ferris as PNG resource

--- pebble-test/src/lib.rs
+++ pebble-test/src/lib.rs
@@ -2,15 +2,15 @@
 #![warn(clippy::pedantic)]
 
 use debugless_unwrap::DebuglessUnwrap as _;
-use pebble::{
-	foundation::app,
+use pebble_skip::{
+	foundation::{app, resources},
 	standard_c::CStr,
 	user_interface::window::number_window::{NumberWindow, NumberWindowData},
 };
 
 #[no_mangle]
 pub extern "C" fn main() -> i32 {
-	let label = CStr::try_from_static("miles to see you\0").unwrap();
+	let label = CStr::try_from_static("PNG chonk\0").unwrap();
 	let number_window = NumberWindow::new(
 		&label,
 		NumberWindowData {
@@ -21,7 +21,11 @@ pub extern "C" fn main() -> i32 {
 		},
 	)
 	.debugless_unwrap();
-	number_window.set_value(10_000);
+
+	let r#unsafe = resources::get_handle(1);
+	let size = resources::size(r#unsafe);
+	#[allow(clippy::cast_possible_truncation, clippy::cast_possible_wrap)]
+	number_window.set_value(size as i32);
 	let window = number_window.window();
 	window.show(true);
 	app::event_loop();
